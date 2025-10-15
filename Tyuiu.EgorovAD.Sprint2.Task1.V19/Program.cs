55ef8ade1b5e856a752c66631c2630abc0278078
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-15 21:57:22 +0300

    Task1

---
+++ Tyuiu.EgorovAD.Sprint2.Task1.V19/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.EgorovAD.Sprint2.Task1.V19.Lib;
+namespace Tyuiu.EgorovAD.Sprint2.Task1.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 696;
+            int b = 354;
+            int c = 423;
+            int d = 957;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.Title = "Спринт #2 | Выполнил: Егоров.А.Д. | СМАРТб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: cоздания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Егоров.А.Д. | СМАРТб25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использоватьодин раз в выражении*");
+            Console.WriteLine("* и логических операций (|, &, ||, &&, !, ^, последовательность операций  *");
+            Console.WriteLine("* не должна нарушаться), а также арифметических выражений, которая вернет *");
+            Console.WriteLine("* последовательность(массив): (True, False, True, False, True, False),    *");
+            Console.WriteLine("* при a = 696, b = 354, c = 423, d = 957                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
