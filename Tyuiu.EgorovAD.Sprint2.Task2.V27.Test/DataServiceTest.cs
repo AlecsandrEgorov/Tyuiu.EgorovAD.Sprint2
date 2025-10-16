8bd7b28bc97960ea03aa18b53664d7f772d183b6
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-16 21:34:28 +0300

    Task2

---
+++ Tyuiu.EgorovAD.Sprint2.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.EgorovAD.Sprint2.Task2.V27.Lib;
+namespace Tyuiu.EgorovAD.Sprint2.Task2.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 11;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint2.Task2.V27/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.EgorovAD.Sprint2.Task2.V27.Lib;
+namespace Tyuiu.EgorovAD.Sprint2.Task2.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Егоров.А.Д. | СМАРТб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: cоздания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Егоров.А.Д. | СМАРТб25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры  *");
+            Console.WriteLine("* и вычисляет находится ли точка с координатами X,Y в заштрихованной      *");
+            Console.WriteLine("* области.                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите значение X:");
+            int x  = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            DataService ds = new DataService();
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Находится в облости");
+            }
+            else
+            {
+                Console.WriteLine("Не находится в облости");
+            }
+            Console.ReadKey();    
+        }
+    }
+}
