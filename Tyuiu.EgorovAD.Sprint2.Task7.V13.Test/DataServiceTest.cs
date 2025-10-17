3b818c3aec489719acb17ac7b2ef4cc35790d9ce
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-17 23:14:05 +0300

    Task7

---
+++ Tyuiu.EgorovAD.Sprint2.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.EgorovAD.Sprint2.Task7.V13.Lib;
+namespace Tyuiu.EgorovAD.Sprint2.Task7.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheck()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.7;
+            double y = 0.7;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint2.Task7.V13/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.EgorovAD.Sprint2.Task7.V13.Lib;
+namespace Tyuiu.EgorovAD.Sprint2.Task7.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите значение X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в закрашенной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка находится в не закрашенной области");
+            }
+            Console.ReadKey();
+        }
+    }
+}
